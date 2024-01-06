a2cb26bd308f1a8295a7642d1d20a022ca3c7ff0
Logan <iml885203@gmail.com>
2024-01-06 15:47:10 +0800

    🎉 new test project

---
+++ BudgetServiceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BudgetServiceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
